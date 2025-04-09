7b0ac099e38b897a6927b4eae8e61dead15c83d8
Om Tiwari <omtiwaripandey@gmail.com>
2025-04-09 13:18:57 +0530

    Initial Commit

---
+++ DinoDex/DinoDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoDexApp.swift
+//  DinoDex
+//
+//  Created by Om Tiwari Pandey on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DinoDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
