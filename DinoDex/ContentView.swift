83dcd2a807b01f3c57ea8d8696eb45aa2b73cdae
Om Tiwari <omtiwaripandey@gmail.com>
2025-04-10 09:10:02 +0530

    setting up basic navigation #1

--- DinoDex/ContentView.swift
+++ DinoDex/ContentView.swift
@@ -10,37 +10,66 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
-    var body: some View {
-        List(predators.apexPredators) { predator in
-            
-            HStack {
-                //Dinosaur image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                VStack(alignment: .leading){
-                    //Name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    //Type
-                    Text(predator.type.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.color)
-                        .cornerRadius(20)
-                    //better would be
-                    // .clipshape(.capsule)
-                        
-                        
-                    
-                    
-                }
+    @State var searchText = ""
+    
+    var filteredDinos: [DinoApexPredator] {
+        if searchText.isEmpty {
+            predators.apexPredators
+        } else {
+            predators.apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
             }
+        }
+        
+    }
+    
+    var body: some View {
+        
+        NavigationStack {
+            List(filteredDinos) { predator in
+                
+                NavigationLink {
+                    
+                    Image(predator.image).resizable().scaledToFit()
+                } label: {
+                    
+                    
+                    
+                    HStack {
+                        //Dinosaur image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        VStack(alignment: .leading){
+                            //Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            //Type
+                            Text(predator.type.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.color)
+                                .cornerRadius(20)
+                            //better would be
+                            // .clipshape(.capsule)
+                            
+                            
+                            
+                            
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle(Text("DinoDex"))
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
             
         }.preferredColorScheme(.dark )
     }
